e6211463562b3b749792d464ea17d11ceb8fb136
Adam Garling <adamgarling@Adams-MacBook-Pro.local>
2018-05-31 12:30:57 -0700

    updated search bar

--- models/userBrewery.js
+++ models/userBrewery.js
@@ -20,6 +20,11 @@ module.exports = function (sequelize, Sequelize) {
 
         userID: {
             type: Sequelize.INTEGER,
+        },
+        visited: {
+            type: Sequelize.BOOLEAN,
+            allowNull: false,
+            defaultValue: false 
         }
 
     });
